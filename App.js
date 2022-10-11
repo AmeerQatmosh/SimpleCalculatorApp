4ba9bf69bfe8d1e9b2f3de6886392280ab52219f
AmeerQatmosh <ameerqsh@gmail.com>
2022-10-11 11:22:32 +0300

    UI

--- App.js
+++ App.js
@@ -6,14 +6,16 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React,{ Component } from 'react';
 import type {Node} from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   useColorScheme,
   View,
 } from 'react-native';
@@ -26,82 +28,57 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Simple Calculator App">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
+export default class ButtonBasics extends Component {
+  _onPressButton() {
+    alert('You tapped the button!')
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.alternativeLayoutButtonContainer}>  
+          <TextInput style={styles.input} placeholder="Primary value" keyboardType="numeric" />
+          <TextInput style={styles.input} placeholder="Secondary value" keyboardType="numeric" />
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+        <TextInput style={styles.output} placeholder="Result" />
+        <View style={styles.alternativeLayoutButtonContainer}>     
+          <Button onPress={this._onPressButton} title="+" color="#841584" />
+          <Button onPress={this._onPressButton} title="-" color="#841584" />
+          <Button onPress={this._onPressButton} title="×" color="#841584" />
+          <Button onPress={this._onPressButton} title="÷" color="#841584" />
+          <Button onPress={this._onPressButton} title="=" color="#841584" />
+        </View>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+   flex: 1,
+   justifyContent: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  buttonContainer: {
+    margin: 70,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  alternativeLayoutButtonContainer: {
+    margin: 70,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
-  highlight: {
-    fontWeight: '700',
+  input: {
+    height: 50,
+    margin: 12,
+    borderWidth: 1,
+    padding: 5,
+  },
+
+  output: {
+    height: 40,
+    margin: 10,
+    borderWidth: 1,
+    padding: 3,
+    justifyContent: 'center',
   },
 });
 
-export default App;
+//export default App;
